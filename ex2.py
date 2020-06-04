139c24a2ae9340df4983c129536a35f0d918dbe4
cortezmb <michael.cortez78@gmail.com>
2020-06-04 08:51:07 -0500

    completed

---
+++ ex2.py
@@ -0,0 +1,6 @@
+#Loop exercise 2
+start = int(input("What number would you like to start on from 1-10? "))
+end = int(input("What number would you like to end on from 1-10 "))
+
+for i in range(start, end):
+    print(i)
